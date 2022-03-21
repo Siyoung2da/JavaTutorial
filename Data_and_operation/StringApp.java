debb4eaf0a0870a4900ff381a18bfea05294c207
Siyoung Park <drq5@daum.net>
2022-03-21 18:19:43 +0900

    ~220321

---
+++ Data_and_operation/StringApp.java
@@ -0,0 +1,17 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World"); // String
+		System.out.println('H');		// '' = character	
+		System.out.println("H");
+		
+		System.out.println("Hello"
+				+ "World");
+		System.out.println("Hello \nWorld");
+		System.out.println("Hello \"World\"");
+		
+	}
+
+}
